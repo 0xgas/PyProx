e5956de9b160b4793da76e0e0e7c957687d203e6
0xgas <0xgas@protonmail.ch>
2017-11-06 19:31:57 +0000

    Asyncio :)

--- pyprox.py
+++ pyprox.py
@@ -1,146 +1,142 @@
 #!/usr/bin/python3
-# PyProx!
+# PyProx! Simple network proxy to analyse what is passing between you and your target.
 
 import sys
+import asyncio
+import logging
 import argparse
-import socket
-import time
-import threading
 
 Verbose = False
+LocalBuffer = b""
 
-
-class PyProx():
+class PyProxLocal(asyncio.Protocol):
 	
-	def __init__(self, args):
-		self.src_host = args.src_host
-		self.src_port = args.src_port
-		self.dst_host = args.dst_host
-		self.dst_port = args.dst_port
-		self.last_time = time.time()
+	def __init__(self, remote):
+		self.remote = remote
+		self.remote_up = False
 
+	def connection_made(self, transport):
+		self.peername = transport.get_extra_info('peername')
+		log("a", "New connection from local - {}".format(self.peername))
+		self.transport = transport
+		asyncio.ensure_future(self.proxy_out_connect()).add_done_callback(self.remote_ready)
+	
+	def remote_ready(self, *args):
+		global LocalBuffer
+		self.remote_up = True
+		if len(LocalBuffer) > 0:
+			self.transport_remote.write(LocalBuffer)
+			LocalBuffer = b""
 
-		self.server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-		self.server.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+	def connection_lost(self, ex):
+		log("w", "Connection lost from local - {} ({})".format(self.peername, ex))
+		self.transport.close()
 
-	def run(self):
-		log("i", "Listening connections on %s:%d" % (self.src_host, self.src_port))
-		self.server.bind((self.src_host, self.src_port))
-		self.server.listen(5)
-		while True:
-			client_data = self.server.accept()
-			log("i", "New connection from %s:%d!" % (client_data[1][0], client_data[1][1]))
-			t= threading.Thread(target=self.proxy_handle, args=(client_data,))
-			t.setDaemon(True)
-			t.start()
-
-
-	def proxy_handle(self, client_data):
-		(client,addr)=client_data
-		self.remote = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-		self.remote.connect((self.dst_host, self.dst_port))
-
-		log("a", "Trying to get data from remote...")
-		remote_buffer = self.recv_from(self.remote)
-		if len(remote_buffer):
-			log("i", "Sending %d bytes to client" % len(remote_buffer))
-			hexdump("<", remote_buffer)
-			client.send(remote_buffer)
+	def data_received(self, data):
+		global LocalBuffer
+		log("i", "Received {} bytes from local".format(len(data)))
+		hexdump(">", data)
+		if not self.remote_up:
+			LocalBuffer+=data
 		else:
-			log("a", "No data from remote, listening client now...")
-		while True:
-			local_buffer = self.recv_from(client)
-			
-			if len(local_buffer):
-				log("i", "Received %d bytes from client" % len(local_buffer))
-				hexdump(">", local_buffer)
-				try:
-					self.remote.send(local_buffer)
-				except BrokenPipeError:
-					log("w", "Lost connection to remote, reconnecting...")
-					self.remote.close()
-					self.remote = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-					self.remote.connect((self.dst_host, self.dst_port))
-					self.remote.send(local_buffer)
-				log("i", "Sent to remote")
-			remote_buffer = self.recv_from(self.remote)
-			if len(remote_buffer):
-				log("i", "Received %d bytes from remote" % len(remote_buffer))
-				hexdump("<", remote_buffer)
-				client.send(remote_buffer)
-				log("i", "Sent to client")
+			self.transport_remote.write(data)
 
-			if not len(local_buffer) and not len(remote_buffer) and (time.time() - self.last_time) > 40:
-				self.close_conns(client)
-				return
+	def eof_received(self):
+		pass
 
-	def recv_from(self, sock, timeout=0.5):
-		buf=b""
-		if timeout is not None:
-			sock.settimeout(timeout)
-		try:
-			while True:
-				data = sock.recv(4096)
-				if not len(data):
-					break
-				buf += data
-				self.last_time = time.time()
-		except:
-			pass
-		return buf
+	async def proxy_out_connect(self):
+		loop = asyncio.get_event_loop()
+		self.transport_remote, proxy_remote = await loop.create_connection(PyProxRemote, self.remote[0], self.remote[1])
+		proxy_remote.transport_local = self.transport
 
-	def close_conns(self, client=None):
-		if client is not None:
-			client.close()
-		self.remote.close()
-		log("i", "No more data, closing connections")
-		return 
 
-def main():
-	global Verbose
-	args=parse_params()
-	Verbose = args.verbose
-	proxy = PyProx(args)
-	proxy.run()
-	return
+class PyProxRemote(asyncio.Protocol):
+	
+	def connection_made(self, transport):
+		self.peername = transport.get_extra_info('peername')
+		log("a", "Connection made to remote {}".format(self.peername))
+		self.transport = transport
+
+	def connection_lost(self, ex):
+		log("w", "Connection lost to remote {} ({})".format(self.peername, ex))
+		self.transport.close()
+
+	def data_received(self, data):
+		global RemoteBuffer
+		log("i", "Received {} bytes from remote".format(len(data)))
+		hexdump("<", data)
+		self.transport_local.write(data)
+	
+	def eof_received(self):
+		pass
+	
+def parse_params():
+	parser = argparse.ArgumentParser()
+	
+	parser.add_argument('-v', '--verbose', help="Verbose mode", action="store_true", required=False)
+	parser.add_argument('src_host', help="src IP to bind", type=str)
+	parser.add_argument('src_port', help="src PORT to bind", type=int)
+	parser.add_argument('dst_host', help="dst IP to listen", type=str)
+	parser.add_argument('dst_port', help="dst PORT to listen", type=int)
+	return parser.parse_args()
+
+
+# logs on stderr, so redirecting stdout to a file only gets you the hexdump, which is cool imo
+def log(mode, msg):
+	if Verbose:
+		if mode == 'e': # Error
+			sys.stderr.write("[\033[31mx\033[0m] ")
+		if mode == 'i': # Info
+			sys.stderr.write("[\033[32m*\033[0m] ")
+		if mode == 'a': # Action
+			sys.stderr.write("[\033[33m+\033[0m] ")
+		if mode == 'w': # Warning
+			sys.stderr.write("[\033[34m!\033[0m] ")
+		sys.stderr.write(str(msg) + "\n")
+		sys.stderr.flush()
 
 
 def hexdump(origin, src, length=16):
 	result = []
 	digits = 2
+	print()
 	for i in range(0, len(src), length):
 		s = bytes(src[i:i+length])
 		hexa = ' '.join(["%0*x" % (digits, x) for x in s])
 		if len(hexa) < length*digits+(length-1):
 			hexa= hexa + (" " * (length*digits+(length-1) - len(hexa)))
 		text = ''.join(["%s" % chr(x) if 0x20 <= x < 0x7F else '.' for x in s])
+		if len(text) < length:
+			text = text + (" " *(length - len(text)))
 		result.append("%s %08x  %-*s  |%s|" % (origin, i, 9, hexa, text))
 	print('\n'.join(result))
 
-def parse_params():
-	parser = argparse.ArgumentParser()
-	parser.add_argument('-v','--verbose', help="Verbose mode", action="store_true")
-	parser.add_argument('-6','--ipv6', help="Use ipv6 for remote host", action="store_true")
-	parser.add_argument('-u','--udp', help="Use UDP proto (Not working yet)", action="store_true")
-	parser.add_argument('src_host', help="src IP to bind", type=str)
-	parser.add_argument('src_port', help="src PORT to bind", type=int)
-	parser.add_argument('dst_host', help="dst IP to listen", type=str)
-	parser.add_argument('dst_port', help="dst PORT to listen", type=int)
 
-	return parser.parse_args()
+def main():
+	global Verbose
+	logging.basicConfig(
+			level=logging.DEBUG,
+			format='%(name)s: %(message)s',
+			stream=sys.stderr,
+	)
+	logR = logging.getLogger('main')
+	loop = asyncio.get_event_loop()
 
-def log(mode, msg):
-	if Verbose:
-		if mode == 'e': # Error
-			sys.stdout.write("[\033[31mx\033[0m] ")
-		if mode == 'i': # Info
-			sys.stdout.write("[\033[32m*\033[0m] ")
-		if mode == 'a': # Action
-			sys.stdout.write("[\033[33m+\033[0m] ")
-		if mode == 'w': # Warning
-			sys.stdout.write("[\033[34m!\033[0m] ")
-		sys.stdout.write(str(msg) + "\n")
-		sys.stdout.flush()
+	args = parse_params()
+	if args.verbose:
+		Verbose = True
+	coro = loop.create_server(lambda: PyProxLocal((args.dst_host, args.dst_port)), args.src_host, args.src_port)
+	server = loop.run_until_complete(coro)
+	try:
+		loop.run_forever()
+	except KeyboardInterrupt:
+		pass
+	finally:
+		log("i", "Closing pyProx")
+		server.close()
+		loop.close()
+
 
 if __name__=='__main__':
 	main()
+
